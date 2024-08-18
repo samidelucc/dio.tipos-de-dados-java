cafab52d9dcc53cc14916b18c82dd96b5c60bd5b
samidelucc <sonicro13@gmail.com>
2024-08-18 02:45:27 -0300

    criação do doc de dicas e dos exemplos no doc java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
+//AULA 3
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //casting
+//AULA 4
+//      variável
+        int numero = 5;
+
+        numero = 10;
+        System.out.println(numero);
+
+//      constante
+        final double VALOR_DE_PI = 3.14; //maiúsculo para indicar constante
+
+        String meuNome = "sami"; //nome
     }
 }
+
+
